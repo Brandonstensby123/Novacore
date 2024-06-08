baaf8041f39e494c98de8c77963b805c975c12ab
Brandonstensby123 <brandonstensby@yahoo.ca>
2024-06-07 21:04:18 -0600

    Update Novacore.py

--- Novacore.py
+++ Novacore.py
@@ -1,1 +1,14 @@
 
+```
+class NovaCore:
+    def __init__(self):
+        self.ideas = []
+
+    def generate_idea(self):
+        idea = "A novel concept: " + str(len(self.ideas) + 1)
+        self.ideas.append(idea)
+        return idea
+
+# Create an instance of the NovaCore
+nova = NovaCore()
+```

f3d5e69d05052bd84fbc3bd13b6cb1a6ca330d24
Brandonstensby123 <brandonstensby@yahoo.ca>
2024-06-07 21:03:06 -0600

    Update Cosmicegg.py

--- Cosmicegg.py
+++ Cosmicegg.py
@@ -1,1 +1,11 @@
 
+class CosmicEgg:
+    def __init__(self):
+        self.potential = "infinite"
+
+    def tap_into_potential(self):
+        return "Unlocking the secrets of the universe..."
+
+# Create an instance of the CosmicEgg
+egg = CosmicEgg()
+```
